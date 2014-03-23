e6e0e39805d466656536d56b2d30b4ef2cd86322
Ilya Epifanov <elijah.epifanov@gmail.com>
2014-03-23 14:15:25 +0400

    Added some useful scans with a usage example and a simple test

---
+++ tests/Main.hs
@@ -0,0 +1,26 @@
+module Main (main) where
+
+import Data.Functor.Identity
+import Test.Framework (defaultMain, testGroup, Test)
+import Test.Framework.Providers.HUnit
+import Test.HUnit hiding (Test)
+import Pipes
+import Pipes.Prelude as P
+import Pipes.Extras
+
+main :: IO ()
+main = defaultMain tests
+
+tests :: [Test]
+tests =
+    [ testGroup "Scans" [
+         testCase "Scan1i works for EMA calculation" testScan1i
+         ]
+    ]
+
+testScan1i :: Assertion
+testScan1i = transduce (scan1i (\l i -> l * α + i * (1 - α))) [2, 0, 0, 1.5] @?= ([2, 1, 0.5, 1] :: [Double])
+  where α = 0.5
+
+transduce :: Pipe a b Identity () -> [a] -> [b]
+transduce pipe xs = toList (each xs >-> pipe)
