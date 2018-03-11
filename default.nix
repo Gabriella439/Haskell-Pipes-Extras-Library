b87bc753004bdfe9ffbe17a16cd89431e95e4e58
Gabriel Gonzalez <Gabriel439@gmail.com>
2018-03-10 18:27:27 -0800

    Version 1.0.12 → 1.0.13

--- default.nix
+++ default.nix
@@ -3,7 +3,7 @@
 }:
 mkDerivation {
   pname = "pipes-extras";
-  version = "1.0.12";
+  version = "1.0.13";
   src = ./.;
   libraryHaskellDepends = [ base foldl lens pipes transformers ];
   testHaskellDepends = [
