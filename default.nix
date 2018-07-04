32f74ed2d4dde4004d6d9fb4e70dbafa6bdf2a2b
Gabriel Gonzalez <gabriel@awakenetworks.com>
2018-07-03 18:50:07 -0700

    Refactor Nix code
    This changes the Nix code to pin `nixpkgs` and also use `readDirectory` to
    simplify dependency changes

--- default.nix
+++ default.nix
@@ -1,14 +1,35 @@
-{ mkDerivation, base, foldl, HUnit, lens, pipes, stdenv
-, test-framework, test-framework-hunit, transformers
-}:
-mkDerivation {
-  pname = "pipes-extras";
-  version = "1.0.14";
-  src = ./.;
-  libraryHaskellDepends = [ base foldl lens pipes transformers ];
-  testHaskellDepends = [
-    base HUnit pipes test-framework test-framework-hunit transformers
-  ];
-  description = "Extra utilities for pipes";
-  license = stdenv.lib.licenses.bsd3;
-}
+let
+  fetchNixpkgs = import ./nix/fetchNixpkgs.nix;
+
+  nixpkgs = fetchNixpkgs {
+    rev = "804060ff9a79ceb0925fe9ef79ddbf564a225d47";
+
+    sha256 = "01pb6p07xawi60kshsxxq1bzn8a0y4s5jjqvhkwps4f5xjmmwav3";
+
+    outputSha256 = "0ga345hgw6v2kzyhvf5kw96hf60mx5pbd9c4qj5q4nan4lr7nkxn";
+  };
+
+  readDirectory = import ./nix/readDirectory.nix;
+
+  config = {
+    packageOverrides = pkgs: {
+      haskellPackages = pkgs.haskellPackages.override {
+        overrides =
+          let
+            manualOverrides = haskellPackagesNew: haskellPackagesOld: {
+            };
+
+          in
+            pkgs.lib.composeExtensions (readDirectory ./nix) manualOverrides;
+      };
+    };
+  };
+
+  pkgs =
+    import nixpkgs { inherit config; };
+
+in
+  { inherit (pkgs.haskellPackages) pipes-extras;
+
+    shell = (pkgs.haskell.lib.doBenchmark pkgs.haskellPackages.pipes-extras).env;
+  }
